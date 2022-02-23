47869dbf9d19502ad3f6ff95b3ec678774c905af
Kyle Brown <kanobrown@gmail.com>
2022-02-22 22:58:28 -0600

    Problems 1-12

--- Week3CodingAssignment.js
+++ Week3CodingAssignment.js
@@ -18,12 +18,12 @@ var names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
   //use a loop to iterate through and calculate average number of letters
 var sum1 = 0;
 for(var i=0;i < names.length; i++){
-  sum += names[i].length;
+  sum1 += names[i].length;
 } console.log (sum1/names.length);
 
   //use a loop to iterate and concatenate all the customerNames
 var con = names[0];
-for(var i = 1, i < names.length; i ++){
+for(var i = 1; i < names.length; i ++){
   con =con.concat(" ", names[i]);
 }  console.log(con);
 
@@ -32,26 +32,52 @@ for(var i = 1, i < names.length; i ++){
   // how do you access the first element of an array?
       //  array[0]
 
-
 //problem 7
- function echo(word,n){
-   return word * n;
- }
+var word = 'Hello'
+var n = 3
+    console.log(word.repeat(n));
+
 
   //problem 8
-  function fullName(firstName, lastName){
-    console.log(firstName + " " + lastName;)
+  var firstName = 'Davey'
+  var lastName = 'Jones'
+  function fullName(a, b){
+    console.log(firstName + " " + lastName);
   }
+  fullName(firstName, lastName);
 
   //problem 9 returns true if sum > 100
+  arry1 = [1, 5, 19, 23, 38, 42, 54]
+
 
   //problem 10 can use array for 9 returns average value for array
+function calculateAverage(array){
+  var total = 0;
+  var count = 0;
+  array.forEach(function(item, index){
+    total += item;
+    count++;
+  });
+  return total/count;
+} console.log(calculateAverage(arry1));
 
   // problem 11 compare array averages return true if first array is larger than 2nd
-
-  //problem 12
-function willBuyDrink(isHotOutside, moneyInPocket){
-  return (isHotOutside && moneyInPocket > 10.50)
+arry2 = [2,6,14,21,39,43,57]
+if (calculateAverage(arry1)> calculateAverage(arry2)){
+  console.log(true)
+} else {
+  console.log(false)
 }
+  //problem 12
+  var isHotOutside = true
+  var moneyInPocket = 11.45
+function willBuyDrink(i,j){
+  if(isHotOutside && moneyInPocket > 10.50){
+    console.log(true)
+  }else {
+    console.log(false)
+  };
+}
+
 
 // problem 13, make your own
